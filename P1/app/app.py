8c208033545034bca194092e76ab4fdf0ab592ab
Marina Muñoz <marinamuca01@gmail.com>
2022-09-23 11:27:51 +0200

    Added fibonacci exercise

--- P1/app/app.py
+++ P1/app/app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/fibonacci/<int:number>')
+def fibonacci(number):
+  sucesion = [0,1]
+
+  for i in range (2, number):
+    suma = sucesion[i-2] + sucesion[i-1]
+    sucesion.append(suma)
+  return 'El término numero ' + str(number) + ' de la sucesión de fibonacci es: ' + str(sucesion[number-1])
