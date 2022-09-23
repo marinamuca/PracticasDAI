eb55a0616831fd84796441a80f4af6977bdfc303
Marina Muñoz <marinamuca01@gmail.com>
2022-09-23 11:58:58 +0200

    Added SquareBrackets exercise

--- P1/app/app.py
+++ P1/app/app.py
@@ -1,7 +1,33 @@
 #./app/app.py
 from math import sqrt
+import random
 from flask import Flask
 app = Flask(__name__)
+
+def generarCadena():
+    corchetes = ["[", "]"]
+    return ''.join(random.choice(corchetes) for x in range(random.randrange(11)))
+
+def corchetes(cadena):
+  # Comprobar si está balanceada
+  balance = 0
+
+  firstOpen = True
+  for c in cadena:
+      if c == "[":
+        balance+=1
+      else :
+          if balance == 0:
+              firstOpen = False
+              break
+          else:
+              balance -=1
+  balanceada = False
+  if (firstOpen and balance == 0):
+      balanceada = True
+  return balanceada
+  # else:
+  #     return "La cadena "+ cadena +" no está balanceada"
 
 @app.route('/')
 def hello_world():
@@ -36,5 +62,23 @@ def eratostenes(n):
   for i in range (2, n+1):
       if not marcados[i-2]:
           result += str(naturales[i-2]) + ' '
-          
+
   return result
+
+@app.route('/corchetes')
+def cochetes_random():
+  cadena = generarCadena()
+  balanceada = corchetes(cadena)
+  if balanceada:
+    return "La cadena "+ cadena +" está balanceada"
+  else:
+    return "La cadena "+ cadena +" no está balanceada"
+
+
+@app.route('/corchetes/<cadena>')
+def cochetes_input(cadena):
+  balanceada = corchetes(cadena)
+  if balanceada:
+    return "La cadena "+ cadena +" está balanceada"
+  else:
+    return "La cadena "+ cadena +" no está balanceada"
