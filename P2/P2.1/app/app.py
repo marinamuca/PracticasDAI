91c1a7cf86d25d0fd5203c163c377f8ffce868d2
Marina Muñoz <marinamuca01@gmail.com>
2022-10-21 10:33:53 +0200

    P2 done

--- P2/P2.1/app/app.py
+++ P2/P2.1/app/app.py
@@ -1,11 +1,13 @@
 #./app/app.py
 from dataclasses import replace
+import json
 from urllib import response
 from xmlrpc.client import ResponseError
 from bson.json_util import dumps
 from pymongo import MongoClient
-
-from flask import Flask, Response
+from bson import ObjectId
+from flask import Flask, Response, request, jsonify
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
@@ -14,6 +16,17 @@ client = MongoClient("mongo", 27017)
 
 # Base de datos
 db = client.cockteles
+
+# PRACTICA 2 - PARTE 1
+
+def buscar(id):
+    buscado = db.recipes.find_one({'_id':ObjectId(id)})
+    if buscado:
+            buscado['_id'] = str(buscado['_id']) # casting a string (es un ObjectId)
+            return jsonify(buscado)
+    else:
+        return jsonify({'error':'Not found'}), 404
+
 
 @app.route('/todas_las_recetas')
 def mongo():
@@ -62,3 +75,82 @@ def receta_compuesta_de(number, nombre_array):
 #     resJson = dumps(response)
 #     return Response(resJson, mimetype='application/json')
 
+
+# PRACTICA 2 - PARTE 2
+
+# para devolver una lista (GET), o añadir (POST)
+@app.route('/api/recipes', methods=['GET', 'POST'])
+def api_1():
+    if request.method == 'GET':
+        lista = []
+        buscados = db.recipes.find().sort('name')
+        for recipe in buscados:
+            recipe['_id'] = str(recipe['_id']) # casting a string (es un ObjectId)
+            lista.append(recipe)
+        return jsonify(lista)
+
+    if request.method == 'POST':
+        creado = db.recipes.insert_one(json.loads(request.data))
+        return dumps(db.recipes.find_one({'_id':creado.inserted_id}))
+         
+           
+# para devolver una, modificar o borrar
+@app.route('/api/recipes/<id>', methods=['GET', 'PUT', 'DELETE'])
+def api_2(id):
+    if request.method == 'GET':
+        ingredient = request.args.get("con")
+        if ingredient:
+            regexpr = "(?i)" + ingredient.replace("_", " ")
+            response = db.recipes.find({ "ingredients": { "$elemMatch": {"name": {"$regex": regexpr }} } })
+            resJson = dumps(response)
+            return Response(resJson, mimetype='application/json')
+        else:
+            return buscar(id)
+    if request.method == 'PUT':
+        db.recipes.update_one({'_id': ObjectId(id)}, {"$set": json.loads(request.data)})
+        return buscar(id)
+
+    if request.method == 'DELETE':
+        eliminado = buscar(id)
+        db.recipes.delete_one({'_id': ObjectId(id)})
+        return eliminado
+     
+# PRACTICA 2 - PARTE 2 usando flask restful
+
+api = Api(app)
+
+class Recipes(Resource):
+    def get(self):
+        ingredient = request.args.get("con")
+        if ingredient:
+            regexpr = "(?i)" + ingredient.replace("_", " ")
+            response = db.recipes.find({ "ingredients": { "$elemMatch": {"name": {"$regex": regexpr }} } })
+            resJson = dumps(response)
+            return Response(resJson, mimetype='application/json')
+        else:
+            lista = []
+            buscados = db.recipes.find().sort('name')
+            for recipe in buscados:
+                recipe['_id'] = str(recipe['_id']) # casting a string (es un ObjectId)
+                lista.append(recipe)
+            return jsonify(lista)
+    def post(self):
+        creado = db.recipes.insert_one(json.loads(request.data))
+        resJson = dumps(db.recipes.find_one({'_id':creado.inserted_id}))
+        return Response(resJson, mimetype='application/json')
+
+class RecipesID(Resource):
+    def get(self, id):
+        return buscar(id)
+
+    def put(self, id):
+        db.recipes.update_one({'_id': ObjectId(id)}, {"$set": json.loads(request.data)})
+        return buscar(id)
+
+    def delete(self, id):
+        eliminado = buscar(id)
+        db.recipes.delete_one({'_id': ObjectId(id)})
+        return eliminado
+
+api.add_resource(Recipes, '/api/v2/recipes')
+api.add_resource(RecipesID, '/api/v2/recipes/<id>')
