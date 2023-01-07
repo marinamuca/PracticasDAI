6a3fc6009b492b494a7e8bf673ef9d62f94cb006
Marina Muñoz <marinamuca01@gmail.com>
2023-01-07 16:11:10 +0100

    Refactor to add new component BlogInfo

---
+++ P5/dojo-blog/src/BlogInfo.js
@@ -0,0 +1,11 @@
+const BlogInfo = ({blog, handleDelete}) => {
+    return (
+        <div className="blog-preview">
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+            <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+        </div>
+    );
+}
+
+export default BlogInfo;
