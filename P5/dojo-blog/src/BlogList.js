2b063a716520bc822ffe1211a6eb8450949f5c2b
Marina Muñoz <marinamuca01@gmail.com>
2023-01-07 17:34:39 +0100

    Added Bootstrap

--- P5/dojo-blog/src/BlogList.js
+++ P5/dojo-blog/src/BlogList.js
@@ -1,12 +1,21 @@
 import BlogPreview from "./BlogPreview";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const BlogList = ({blogs, title, handleDelete}) => {
     return (
         <div className="blog-list">
-            <h2>{ title }</h2>
-            {blogs.map((blog) => (
-                <BlogPreview blog={blog} handleDelete={handleDelete} key={blog.id}/>
-            ))}
+            <h2>{title}</h2>
+            <Container>
+                <Row>
+                    {blogs.map((blog) => (
+                        <Col xs={6}>
+                            <BlogPreview blog={blog} handleDelete={handleDelete} key={blog.id} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container> 
         </div>
     );
 }
--- P5/dojo-blog/src/BlogPreview.js
+++ P5/dojo-blog/src/BlogPreview.js
@@ -1,10 +1,41 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react'
+
 const BlogPreview = ({blog, handleDelete}) => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <div className="blog-preview">
-            <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
-            <button onClick={() => handleDelete(blog.id)}>delete blog</button>
-        </div>
+        <>
+            <Card className='blog-preview' style={{ minWidth: '100%' }}>
+                <Card.Body>
+                    <Card.Title>
+                        <h2>{blog.title}</h2>
+                    </Card.Title>
+                    <Card.Text>
+                        <p>Written by {blog.author}</p>
+                    </Card.Text>
+                    <Button variant="danger" onClick={handleShow}>Delete Blog</Button>
+                </Card.Body>
+            </Card>
+
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Are you Sure?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Do you really want to delete {blog.title}?</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Cancel</Button>
+                    <Button variant="danger" onClick={() => handleDelete(blog.id)}>Delete Blog</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
 
